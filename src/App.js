8e0b47b16fdedd8240bc7fd0f41419d1f3eb3b7d
prashant-2309 <singh2309prashant@gmail.com>
2023-08-23 18:49:54 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+const uuid = require('uuid');
 
 function App() {
+  const [image, setImage] = useState(null);
+  const [uploadResultMessage, setUploadResultMessage] = useState('Please upload an image to authenticate.');
+  const [visitorName, setVisitorName] = useState('placeholder.jpeg');
+  const [isAuth, setAuth] = useState(false);
+
+  async function sendImage(e) {
+    e.preventDefault();
+    setVisitorName(image.name);
+    const visitorImageName = uuid.v4();
+    
+    const imageUrl = `https://ys28bydtsj.execute-api.us-east-1.amazonaws.com/dev/prashant-visitor-image/${visitorImageName}.jpeg`;
+    
+    try {
+      await fetch(imageUrl, {
+        method: 'PUT',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'image/jpeg'
+        },
+        body: image
+      });
+
+      const response = await authenticate(visitorImageName);
+      if (response && response.Message === 'Success') {
+        setAuth(true);
+        setUploadResultMessage(`Hi ${response.firstName} ${response.lastName}, welcome to work. Hope you have a productive day today!!`);
+      } else {
+        setAuth(false);
+        setUploadResultMessage('Authentication Failed: this person is not an employee.');
+      }
+    } catch (error) {
+      setAuth(false);
+      setUploadResultMessage('There is an error during the authentication process. Please try again.');
+      console.error(error);
+    }
+  }
+
+  async function authenticate(visitorImageName) {
+    const requestUrl = `https://ys28bydtsj.execute-api.us-east-1.amazonaws.com/dev/employee?objectKey=${visitorImageName}.jpeg`;
+    try {
+      const response = await fetch(requestUrl, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Prashant's Facial Recognition System</h2>
+      <form onSubmit={sendImage}>
+        <input type="file" name="image" onChange={e => setImage(e.target.files[0])} />
+        <button type="submit">Authenticate</button>
+      </form>
+      <div className={isAuth ? 'success' : 'failure'}>{uploadResultMessage}</div>
+      <img src={require(`./visitors/${visitorName}`)} alt="Visitor" height={250} width={250} />
     </div>
   );
 }
